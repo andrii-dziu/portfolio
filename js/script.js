9040f39e833e2a0b302347f4ed1f529aa0e3dbb6
Andrii <dzura1996das@gmail.com>
2021-12-02 11:50:13 +0200

    modifided adaptive menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.hidden');
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".hidden-menu");
+let linkItem = document.querySelectorAll(".link-hide");
 
-burger.addEventListener('click', function(event) {
-    nav.classList.toggle('active');
+burger.addEventListener("click", function (event) {
+  menu.classList.toggle("active");
 });
+
+linkItem.forEach((link) =>
+  link.addEventListener("click", function (event) {
+    menu.classList.toggle("active");
+  })
+);
